707d2b08c9e6498f906f5ea295d0d8bf2db0af13
Mihail Sinkov <mihailsin88@gmail.com>
2021-10-31 21:44:53 +0200

    some progress in 3rd task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,6 @@
 const form = document.querySelector('.form');
-console.dir(form);
-const delayField = form.elements.delay;
-const stepField = form.elements.delay;
-const amountField = form.elements.amount;
+// const button = document.querySelector('button[type="submit"]');
+form.addEventListener('submit', onSubmit);
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
@@ -16,39 +14,18 @@ function createPromise(position, delay) {
     }, delay);
   });
 }
-
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
-
-// const promise = new Promise((resolve, reject) => {
-//   const isTrue = Math.random() > 0.5;
-//   setTimeout(() => {
-//     if (isTrue) {
-//       resolve(5);
-//     } else {
-//       reject('Error!');
-//     }
-//   }, 2000);
-// });
-// promise
-//   .then(value => {
-//     console.log(value);
-//     return value * 2;
-//   })
-//   .then(value => {
-//     console.log(value);
-//     return value * 2;
-//   })
-//   .then(value => {
-//     console.log(value);
-//     return console.log(value * 2);
-//   })
-//   .catch(error => {
-//     console.log(error);
-//   })
-//   .finally(() => console.log('Youll see this log anyway'));
+function onSubmit(e) {
+  e.preventDefault();
+  let delay = form.elements.delay.value;
+  let step = form.elements.step.value;
+  let position = form.elements.amount.value;
+  for (let i = 1; i <= position; i += 1) {
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+}
